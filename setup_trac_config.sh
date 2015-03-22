4124966ca9080cbb9239a1485c607f6f8352a3c6
Mario Mohr <mario_mohr@web.de>
2015-03-22 18:50:32 +0100

    Update setup_trac_config.sh

--- setup_trac_config.sh
+++ setup_trac_config.sh
@@ -1,6 +1,7 @@
 setup_components() {
-    trac-admin /trac config set components trac.web.auth.LoginModule disabled 
+    trac-admin /trac config set components trac.web.auth.LoginModule disabled
     trac-admin /trac config set components acct_mgr.web_ui.* enabled 
+    trac-admin /trac config set components acct_mgr.web_ui.EmailVerificationModule disabled
     trac-admin /trac config set components acct_mgr.admin.* enabled 
     trac-admin /trac config set components acct_mgr.register.* enabled 
     trac-admin /trac config set components acct_mgr.notification.* enabled 
